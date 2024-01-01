969a5c763390413aa312e0cd25c67c8ef96b2f49
FistCoder <88001087+FistCoder@users.noreply.github.com>
2024-01-01 22:06:24 +0100

    added basic content
    cleaned the page, created utility classes for JS, stylised Side Menu,
    added and structured the hero
    
    To Do: fix the bug with the visited links

--- index.php
+++ index.php
@@ -1,36 +1,72 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="reset.css"></link>
-  <link rel="stylesheet" href="style.css"></link>
+  <link rel="stylesheet" href="reset.css">
+  </link>
+  <link rel="stylesheet" href="style.css">
+  </link>
   <title>Music Timeline</title>
 </head>
 <!-- DONT FORGET TO DELETE ALL THE "SEE" classes """ctrl F""" -->
-<body class="">
-  <div class="navbar_menu see">
-  <!-- the menu itself -->
-  </div>
+
+<body>
   <nav class="navbar see">
     <a class="navbar_logo see" href="#">
-      <img src="logo.png" alt="Music Timeline">
+      <img src="assets/images/logo.png" alt="Music Timeline">
     </a>
     <div class="navbar_burger see">
       <!-- TO DO, 3 SVG lines animation across the screen -->
     </div>
+    <div class="navbar_menu see">
+      <!-- the menu itself -->
+      <div class="navbar_menu_title"> <span>Chose the Era you are interested in</span></div>
+      <div class="navbar_menu_divider"></div>
+      <div class="navbar_menu_links">
+        <ul class="navbar_menu_links_list">
+          <li class="navbar_menu_links_list_link"><a href="#test">The Beginning</a></li>
+          <li class="navbar_menu_links_list_link"><a href="#">The Jazz Era</a></li>
+          <li class="navbar_menu_links_list_link"><a href="#">The Rythm and Blues Era</a></li>
+          <li class="navbar_menu_links_list_link"><a href="#">The Rap and Hip-Hop Era</a></li>
+        </ul>
+        <div class="navbar_menu_divider"></div>
+        <div class="navbar_menu_infos"><a href="https://github.com/FistCoder/music-timeline">Repo Here</a></div>
+      </div>
+    </div>
   </nav>
-  <div class="overlay">
-    <main class="main">
-      <!-- dont forget to container this  -->
-      <section style="width: 20rem; height: 140rem;">dude</section>
-    </main>
-  </div>
-  
+  <main class="main">
+    <hero class="main_hero">
+      <div class="main_hero_text">
+        <p>
+          Over the years the music has changed very much <br>
+          Never before we have seen so many choices, <br>
+          from <span class="soft-text">calming and relaxing</span> <br>
+          to <span class="hard-text">aggressive and loud</span><br>
+          and everything in between <br>
+          Let&#39;s dive deep into how the <br>
+          music genres evolved over the years
+        </p>
+      </div>
+      <div class="main_hero_magazine see">
+        <img class="main_hero_magazine_image" src="assets/images/magazine.png" alt="Image of a old Vintage Guitar Magazine">
+        <div class="main_hero_magazine_card main_hero_magazine_card--dark"></div>
+        <div class="main_hero_magazine_card main_hero_magazine_card--light"></div>
+      </div>
+    </hero>
+    <!-- dont forget to container this  -->
+    <section class="main_section_one" id="test">dude</section>
+  </main>
   <script src="script.js"></script>
 </body>
+
 </html>
 
-<!-- TO DO::: side menu for now is fixed to the top of the page, 
-  but putting position: sticky on it, it takes space on the page, 
-  which is not wanted -->
+<!-- TO DO 
+
+
+
+
+
+-->
--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 console.log("✅ Connected")
+
+// motion blur for the side menu https://css-tricks.com/how-to-create-a-realistic-motion-blur-with-css-transitions/
