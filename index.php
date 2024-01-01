8d1cc769e6757f28ad2a9725ec677192fde6cda2
FistCoder <88001087+FistCoder@users.noreply.github.com>
2024-01-01 12:30:14 +0100

    new   :   Navbar + structure :tada:

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="reset.css"></link>
+  <link rel="stylesheet" href="style.css"></link>
+  <title>Music Timeline</title>
+</head>
+<!-- DONT FORGET TO DELETE ALL THE "SEE" classes """ctrl F""" -->
+<body class="">
+  <div class="navbar_menu see">
+  <!-- the menu itself -->
+  </div>
+  <nav class="navbar see">
+    <a class="navbar_logo see" href="#">
+      <img src="logo.png" alt="Music Timeline">
+    </a>
+    <div class="navbar_burger see">
+      <!-- TO DO, 3 SVG lines animation across the screen -->
+    </div>
+  </nav>
+  <div class="overlay">
+    <main class="main">
+      <!-- dont forget to container this  -->
+      <section style="width: 20rem; height: 140rem;">dude</section>
+    </main>
+  </div>
+  
+  <script src="script.js"></script>
+</body>
+</html>
+
+<!-- TO DO::: side menu for now is fixed to the top of the page, 
+  but putting position: sticky on it, it takes space on the page, 
+  which is not wanted -->
