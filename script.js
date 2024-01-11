24e1900463aa258283f147b7b5701c24a543f99b
FistCoder <88001087+FistCoder@users.noreply.github.com>
2024-01-11 14:59:11 +0100

    full sideMenu support + parallax effect

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
-console.log("✅ Connected")
+console.log("✅ Connected");
 
-// motion blur for the side menu https://css-tricks.com/how-to-create-a-realistic-motion-blur-with-css-transitions/
+let burger = document.getElementById("burger");
+let menu = document.getElementById("sideMenu");
+let main = document.getElementById("main");
+let body = document.getElementById("body");
+let links = Array.from(
+  document.getElementsByClassName("navbar_menu_links_list_link")
+); /** Creating an array from the HTMLcollection and passing it to 'links'*/
 
-let burger = document.getElementById("burger")
-let menu = document.getElementById("sideMenu")
-let main = document.getElementById("main")
-let body = document.getElementById("body")
-// BugHunter =>  Dimitri
+// For each link from the 'links' array add an event listener and execute menuToggle()
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    menuToggle();
+  });
+});
+
+// Listen for clicks of the burger and execute menuToggle() each time it is clicked
 burger.addEventListener("click", () => {
-    if (menu.getAttribute("data-toggle") == "closed") {
-        menu.setAttribute("data-toggle", "open")
-        menu.style.right = "0"
-        main.classList.toggle("blur")
-        menu.classList.toggle("blur")
-        
-        // body.classList.toggle("no-scroll")
-        console.log("opened")
+  menuToggle();
+});
 
-    } else if (menu.getAttribute("data-toggle") =="open") {
-        menu.setAttribute("data-toggle", "closed")
-        menu.style.right = "-100%"
-        main.classList.toggle("blur")
-        menu.classList.toggle("blur")
-        // body.classList.toggle("no-scroll")
-        console.log("closed")
-
-    }
-})
+function menuToggle() {
+  if (menu.getAttribute("data-toggle") == "closed") {
+    /* execute if the menu is closed */
+    menu.setAttribute("data-toggle", "open");
+    burger.style.left = "0";
+    menu.style.left = "calc(100% / 3 * 2)";
+    main.classList.toggle("blur");
+    menu.classList.toggle("blur");
+    console.log("opened");
+  } else if (menu.getAttribute("data-toggle") == "open") {
+    /* execute if the menu is open */
+    menu.setAttribute("data-toggle", "closed");
+    burger.style.left = "calc(100% - 10vh)";
+    menu.style.left = "105%";
+    main.classList.toggle("blur");
+    menu.classList.toggle("blur");
+    console.log("closed");
+  }
+}
